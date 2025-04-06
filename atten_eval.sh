33261283397e3bcfe04e2a46118a5d765203c2d6
JiananXie <12110714@mail.sustech.edu.cn>
2025-04-06 17:13:13 +0800

    implement llava-phi2

--- atten_eval.sh
+++ atten_eval.sh
@@ -5,13 +5,21 @@ model_path="models/llava-phi2"
 image_file="view_336.jpg"
 query="What are the things I should be cautious about when I visit this place? Are there any dangerous areas or activities I should avoid? Or any other important information I should know?"
 
-
-if [[ $model_path == *"llava"* ]]; then 
+if [[ $model_path == *"llava-phi"* ]]; then 
+    # conda activate llava_phi
+    CUDA_VISIBLE_DEVICES=0 python run/run_llava_phi.py \
+        --model-path $model_path \
+        --image-file $image_file \
+        --query "$query"
+elif [[ $model_path == *"llava"* ]]; then 
+    # conda activate llava-series
     CUDA_VISIBLE_DEVICES=0 python run/run_llava.py \
         --model-path $model_path \
         --image-file $image_file \
         --query "$query"
 elif [[ $model_path == *"bunny"* ]]; then 
+    # conda activate llava-series
+    # and modify model-type
     CUDA_VISIBLE_DEVICES=0 python run/run_bunny.py \
         --model-path $model_path \
         --model-type "stablelm-2" \
@@ -30,4 +38,6 @@ elif [[ $model_path == *"qwen2"* ]]; then
         --model-path $model_path \
         --image-file $image_file \
         --query "$query"
+else 
+    echo "Unsupported model"
 fi
